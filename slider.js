e1bc471a65120188822508130f421dc5d1281937
Monika <moniika.chmielewska@gmail.com>
2020-06-02 00:16:17 +0200

    Product page. Product slider

--- slider.js
+++ slider.js
@@ -78,4 +78,60 @@ $(document).ready(function(){
             }
         ]
     });
+
+    $('#featured__slider_two').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                arrows: false
+            }
+            }
+        ]
+    });
+
+    $('#slider_for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        vertical: false,
+        fade: true,
+        asNavFor: '#slider_nav',
+        responsive: [
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                dots: false,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+            }
+        ]
+      });
+
+    $('#slider_nav').slick({
+        vertical: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '#slider_for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+      });
+
 });
