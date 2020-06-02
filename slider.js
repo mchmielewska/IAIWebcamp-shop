4db700b95e72a23dc21149d3ae7da7df304e6152
Monika <moniika.chmielewska@gmail.com>
2020-06-03 00:39:22 +0200

    Product page

--- slider.js
+++ slider.js
@@ -134,4 +134,26 @@ $(document).ready(function(){
         focusOnSelect: true
       });
 
+    
+      $('#additional_slider').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                arrows: false
+            }
+            }
+        ]
+    });
+
 });
